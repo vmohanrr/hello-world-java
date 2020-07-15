1f03721d3698077ba684fa3e288a288af71ec1d6
madhu <ybmadhu@gmail.com>
2020-07-15 16:31:55 +0000

    added helloworld class

--- HelloWorld.java
+++ HelloWorld.java
@@ -36,5 +36,7 @@ public class HelloWorld {
   public static void main(String[] args) {
     System.out.println("Hello, World");
     System.out.println("Hello, DevOps");
+    System.out.println("Hi Corona");
+    System.out.println("Hi covid19");
   }
 }
